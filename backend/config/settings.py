fcc8efba75a8944e78582d870881eaf16f92befa
jprepa <164268561+jprepa@users.noreply.github.com>
2026-02-05 20:53:06 -0300

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -81,11 +81,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# Configuração antiga (Comente ou apague)
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# NOVA CONFIGURAÇÃO (PostgreSQL)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Se não tiver URL configurada (local), usa SQLite. Se tiver (Render), usa Postgres.
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 
